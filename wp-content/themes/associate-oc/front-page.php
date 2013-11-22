75f838ce477eeee66c2c508166f1193f0d71832d
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-11-22 15:32:44 -0500

    add some support hardware and fix the title on the home page.

---
+++ wp-content/themes/associate-oc/front-page.php
@@ -0,0 +1,3 @@
+<?php
+remove_action('genesis_post_title','genesis_do_post_title');
+genesis();
--- wp-content/themes/associate-oc/functions.php
+++ wp-content/themes/associate-oc/functions.php
@@ -109,4 +109,24 @@ function msdlab_add_scripts() {
             //wp_enqueue_script('msd-homepage-jquery',get_stylesheet_directory_uri().'/lib/js/homepage-jquery.js',array('jquery','bootstrap-jquery'));
         }
     }
+    }
+
+/*
+* A useful troubleshooting function. Displays arrays in an easy to follow format in a textarea.
+*/
+if(!function_exists('ts_data')){
+    function ts_data($data){
+        $ret = '<textarea class="troubleshoot" rows="20" cols="100">';
+        $ret .= print_r($data,true);
+        $ret .= '</textarea>';
+        print $ret;
+    }
+}
+/*
+* A useful troubleshooting function. Dumps variable info in an easy to follow format in a textarea.
+*/
+if(!function_exists('ts_var')){
+    function ts_var($var){
+        ts_data(var_export( $var , true ));
+    }
 }
